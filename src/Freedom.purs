67270237d0965f2d604234997eef32fe64e6c738
Shinya Takahashi <s.takahashi313@gmail.com>
2019-02-28 23:21:56 +0900

    Added initializer

---
+++ src/Freedom.purs
@@ -0,0 +1,35 @@
+module Freedom
+  ( Config
+  , run
+  ) where
+
+import Prelude
+
+import Control.Monad.Free.Trans (runFreeT)
+import Data.Foldable (sequence_)
+import Freedom.Renderer (createRenderer, render)
+import Freedom.Store (Query, createStore, query, subscribe)
+import Freedom.Styler (createStyler)
+import Freedom.Subscription (Subscription, runSubscription)
+import Freedom.TransformF.Type (TransformF)
+import Freedom.VNode (VNode)
+import Effect (Effect)
+
+type Config f state =
+  { selector :: String
+  , initialState :: state
+  , view :: state -> VNode f state
+  , subscriptions :: Array (Subscription f state)
+  , transformF :: Query state -> TransformF f state
+  }
+
+run :: forall f state. Functor (f state) => Config f state -> Effect Unit
+run { selector, initialState, view, subscriptions, transformF } = do
+  styler <- createStyler
+  store <- createStore initialState
+  let query' = query store
+      transformF' = transformF query'
+  sequence_ $ runSubscription (runFreeT transformF') <$> subscriptions
+  renderer <- createRenderer selector view transformF' query'.select styler
+  subscribe (render renderer) store
+  render renderer
