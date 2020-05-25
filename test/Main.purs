959e91617e6fc6a6eea1a4a892e1b7a7db05108e
Tim de Putter <tim.de.putter83@googlemail.com>
2020-05-25 13:44:40 +0200

    Example unit test

--- test/Main.purs
+++ test/Main.purs
@@ -6,10 +6,13 @@ import Effect (Effect)
 import Test.UI.Diff (testDiff)
 import Test.UI.Eq (testVNodeEq)
 import Test.UI.Show (testVNodeShow)
+import Test.UI.Example (testView)
 import Test.Unit.Main (runTest)
+
 
 main :: Effect Unit
 main = runTest do
   testDiff
   testVNodeEq
   testVNodeShow
+  testView
